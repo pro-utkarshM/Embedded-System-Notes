796315c38ef63b6c910b079d10fa18b9e1b54d66
pro-utkarshM <projects.utkarshMaurya@gmail.com>
2023-08-28 00:04:17 +0530

    Adding resourses for languages

---
+++ Languages/6.s096-january-iap-2013/static_resources/23c7e004be623fe6f46762154c908d8f_rps.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Tool {
+	/* Fill in */
+};
+
+/*
+	Implement class Scissors
+*/
+
+/*
+	Implement class Paper
+*/
+
+/*
+	Implement class Rock
+*/
+
+int main() {
+	// Example main function
+	// You may add your own testing code if you like
+
+	Scissors s1(5);
+	Paper p1(7);
+	Rock r1(15);
+	cout << s1.fight(p1) << p1.fight(s1) << endl;
+	cout << p1.fight(r1) << r1.fight(p1) << endl;
+	cout << r1.fight(s1) << s1.fight(r1) << endl;
+
+	return 0;
+}
